f5ab3de751d7094bc5208c5434e612224ac651d7
noahbachmann <noah_bachmann@sluz.ch>
2026-03-31 08:51:30 +0200

    :trollface: gcd fixes

--- gcd.ts
+++ gcd.ts
@@ -1,6 +1,20 @@
 export class GCD {
 	public static BruteForce(a: number, b: number): number {
-		let i = Math.min(a / 2, b / 2);
+		if (b === 0) {
+			throw new Error(`can't divide by 0`);
+		}
+		if (a === 0) {
+			return 0;
+		}
+
+		a = Math.abs(a)
+		b = Math.abs(b)
+		
+		if(a === 1 || b === 1) {
+			return 1;
+		}
+
+		let i = Math.min(a, b);
 		while (i > 1) {
 			if (a % i == 0 && b % i == 0) {
 				return i;
--- gcd_test.ts
+++ gcd_test.ts
@@ -1,10 +1,23 @@
-import { assertEquals } from "@std/assert/equals";
+import { assertEquals, assertThrows } from "@std/assert";
 import { GCD } from "./gcd.ts";
 
-Deno.test("ggT of 9 and 6 is 3", () => {
+Deno.test("ggT of 12 and 9 is 3", () => {
 	//Arrange
-	const check = GCD.BruteForce(9, 6);
+	const check1 = GCD.BruteForce(12, 9);
+	
+	//Assert
+	assertEquals(check1, 3);
+});
+
+Deno.test("ggT of 7 and 5 is 1, add other special GCD-checks for 100% coverage", () => {
+	//Arrange
+	const check1 = GCD.BruteForce(7, 5);
+	const check2 = GCD.BruteForce(6, 1);
+	const check3 = GCD.BruteForce(0, 6);
 
 	//Assert
-	assertEquals(check, 3);
-});
+	assertEquals(check1, 1);
+	assertEquals(check2, 1);
+	assertEquals(check3, 0);
+	assertThrows(() => GCD.BruteForce(6, 0), `can't divide by 0`);
+})
