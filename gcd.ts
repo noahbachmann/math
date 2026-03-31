b6959463d83ea9a933bc36dffb90f8e8e90b66fd
noahbachmann <noah_bachmann@sluz.ch>
2026-03-31 09:30:30 +0200

    :finnadie: add euclid

--- gcd.ts
+++ gcd.ts
@@ -1,20 +1,12 @@
 export class GCD {
 	public static BruteForce(a: number, b: number): number {
-		if (b === 0) {
-			throw new Error(`can't divide by 0`);
-		}
-		if (a === 0) {
+		const pre = this.firstCheck(a, b);
+		if (pre == null) {
 			return 0;
 		}
+		[a, b] = pre;
 
-		a = Math.abs(a)
-		b = Math.abs(b)
-		
-		if(a === 1 || b === 1) {
-			return 1;
-		}
-
-		let i = Math.min(a, b);
+		let i = a;
 		while (i > 1) {
 			if (a % i == 0 && b % i == 0) {
 				return i;
@@ -22,5 +14,40 @@ export class GCD {
 			i--;
 		}
 		return i;
+	}
+
+	public static euclid(a: number, b: number): number {
+		const pre = this.firstCheck(a, b)
+		if (pre === null) {
+			return 0;
+		}
+		[a, b] = pre;
+
+		while (a != b) {
+			const c = b - a;
+			if (a > c) {
+				b = a;
+				a = c;
+			} else {
+				b = c;
+			}
+		}
+		return a;
+	}
+
+	private static firstCheck(a: number, b: number): [number, number] | null  {
+		if (b === 0) {
+			throw new Error(`can't divide by 0`);
+		}
+		if (a === 0) {
+			return null;
+		}
+
+		a = Math.abs(a);
+		b = Math.abs(b);
+		if (a <= b) {
+			return [a, b];
+		}
+		return [b, a];
 	}
 }
--- gcd_test.ts
+++ gcd_test.ts
@@ -21,3 +21,12 @@ Deno.test("ggT of 7 and 5 is 1, add other special GCD-checks for 100% coverage",
 	assertEquals(check3, 0);
 	assertThrows(() => GCD.BruteForce(6, 0), `can't divide by 0`);
 })
+
+Deno.test("ggT of 12 and 9 should be 3", () => {
+	//Arrange
+	const check1 = GCD.euclid(12, 9);
+	const check2 = GCD.euclid(0, 12);
+	//Assert
+	assertEquals(check1, 3);
+	assertEquals(check2, 0);
+});
